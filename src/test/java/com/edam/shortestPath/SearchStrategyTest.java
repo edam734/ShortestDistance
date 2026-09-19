2f3713f609280999ca2c3df35b522f3395062c88
Eduardo <edam734@gmail.com>
2026-09-20 00:01:58 +0100

    Add test for two-digit grid coordinates.

--- src/test/java/com/edam/shortestPath/SearchStrategyTest.java
+++ src/test/java/com/edam/shortestPath/SearchStrategyTest.java
@@ -7,6 +7,8 @@ import com.edam.shortestPath.tree.SearchShortestTree;
 import com.edam.shortestPath.tree.TreeBuilder;
 import org.junit.jupiter.api.Test;
 
+import java.util.Arrays;
+
 import static org.junit.jupiter.api.Assertions.assertEquals;
 
 class SearchStrategyTest {
@@ -73,5 +75,34 @@ class SearchStrategyTest {
 
         searchTree.setStrategy(new DepthFirstSearchStrategy());
         assertEquals(1, searchTree.minDistance());
+    }
+
+    @Test
+    void shouldHandleCoordinatesWithTwoDigits() {
+        char[][] grid = new char[12][12];
+
+        for (char[] chars : grid) {
+            Arrays.fill(chars, '0');
+        }
+
+        for (int column = 1; column <= 11; column++) {
+            grid[1][column] = '*';
+        }
+
+        for (int line = 1; line <= 11; line++) {
+            grid[line][11] = '*';
+        }
+
+        grid[1][1] = 's';
+        grid[11][11] = 'd';
+
+        Node root = new TreeBuilder(grid).build();
+        SearchShortestTree searchTree = new SearchShortestTree(root);
+
+        searchTree.setStrategy(new BreadthFirstSearchStrategy());
+        assertEquals(20, searchTree.minDistance());
+
+        searchTree.setStrategy(new DepthFirstSearchStrategy());
+        assertEquals(20, searchTree.minDistance());
     }
 }
