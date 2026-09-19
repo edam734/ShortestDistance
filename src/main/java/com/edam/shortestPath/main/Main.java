f8c0d8745c39ab9576568af3faf494394d6ba228
Eduardo <edam734@gmail.com>
2026-09-20 00:25:49 +0100

    "Simplify Main to demonstrate search strategies.

--- src/main/java/com/edam/shortestPath/main/Main.java
+++ src/main/java/com/edam/shortestPath/main/Main.java
@@ -9,29 +9,20 @@ import com.edam.shortestPath.tree.TreeBuilder;
 public class Main {
 
     public static void main(String[] args) {
-        char[][] grid = {{'0', '*', '0', 's'}, {'*', '0', '*', '*'}, {'0', '*', '*', '*'}, {'d', '*', '*', '*'}};
-        char[][] grid2 = {{'0', 's'}, {'d', '*'}};
-        char[][] grid3 = {{'*', '*', 's'}, {'*', 'd', '*'},};
-        char[][] grid4 = {{'*', '*', 's'}, {'d', '*', '*'},};
+        char[][] grid = {
+                {'s', '*', '*', '*'},
+                {'0', '0', '*', '0'},
+                {'*', '*', '*', '*'},
+                {'*', '0', '0', 'd'}
+        };
 
         Node root = new TreeBuilder(grid).build();
         SearchShortestTree searchTree = new SearchShortestTree(root);
-        searchTree.setStrategy(new DepthFirstSearchStrategy());
-        int outputDTS = searchTree.minDistance();
-        System.out.println("outputDTS: " + outputDTS);
-        searchTree.setStrategy(new BreadthFirstSearchStrategy());
-        int outputBFS = searchTree.minDistance();
-        System.out.println("outputBFS: " + outputBFS);
 
-        // teste
-        //		Node n1 = new Node('a', 1, 0);
-        //		n1.parent = null;
-        //		Node n2 = new Node('b', 4, 5);
-        //		n2.parent = n1;
-        //		Node n3 = new Node('c', 5, 5);
-        //		n3.parent = n2;
-        //		Node n4 = new Node('d', 1, 1);
-        //		n4.parent = n3;
-        //		System.out.println(n1.isAlreadyInTree(n3));
+        searchTree.setStrategy(new BreadthFirstSearchStrategy());
+        System.out.println("BFS distance: " + searchTree.minDistance());
+
+        searchTree.setStrategy(new DepthFirstSearchStrategy());
+        System.out.println("DFS distance: " + searchTree.minDistance());
     }
 }
